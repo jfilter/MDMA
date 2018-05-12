6bb733e2b8ed0e3c37761562991e604f1840a31c
Johannes Filter <hi@jfilter.de>
2018-05-12 23:51:33 +0200

    add job reset to admin menu

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User, StyleImage
+from .models import STATUS_WATING, InputImage, Job, StyleImage, User
 
 admin.site.register(User, UserAdmin)
 admin.site.register(StyleImage)
+admin.site.register(InputImage)
+
+
+def reset_status(modeladmin, request, queryset):
+    queryset.update(status=STATUS_WATING, job_started_at=None,
+                    job_finished_at=None, output_image=None)
+
+
+reset_status.short_description = "reset status"
+
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['created_at', 'status',
+                    'job_started_at', 'job_finished_at']
+    ordering = ['-created_at']
+    actions = [reset_status]
+
+
+admin.site.register(Job, JobAdmin)
