4ac6291f8dbc14842460ef81fce083eff5a65c77
Johannes Filter <hi@jfilter.de>
2018-05-16 16:58:22 +0200

    use sentry for error logs

--- mdma/settings.py
+++ mdma/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'raven.contrib.django.raven_compat',
-    'main',
-    'social_django'
+    'social_django',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -199,21 +199,50 @@ USE_TZ = True
 
 SITE_ID = 1
 
-# type: Dict[str, Union[None, str]]
+# for sentry
 RAVEN_CONFIG = {"dsn": os.getenv("RAVEN_DSN")}
 
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
         },
     },
+    'handlers': {
+        'sentry': {
+            # To capture more than ERROR, change to WARNING, INFO, etc.
+            'level': 'ERROR',
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+            'tags': {'custom-tag': 'x'},
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
     'loggers': {
-        'django': {
+        'django.db.backends': {
+            'level': 'ERROR',
             'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
         },
     },
 }
